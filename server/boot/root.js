60475f310fbf15080d8835b6e5bcfcd6df5045b7
Tekeste G <tekeste.kidanu@gmail.com>
2016-09-26 21:32:26 -0500

    modify root url output

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,9 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  // router.get('/', server.loopback.status());
+  router.get('/', function(req, res) {
+  	res.send({blah: false})
+  })
   server.use(router);
 };
