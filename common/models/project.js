bfc3a75ed782c84d812a6debb88e7594035a28b0
Tekeste G <tekeste.kidanu@gmail.com>
2016-09-26 21:33:06 -0500

    finish project model

--- common/models/project.js
+++ common/models/project.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Project) {
+	Project.slug = function(slug, cb) {
+		Project.find({where:{slug:slug}}, function(err, project) {
+			cb(null, project);
+		})
+	}
 
+	Project.remoteMethod(
+        'slug', 
+        {
+          accepts: {arg: 'slug', type: 'string', required:true},
+          http: {path: '/slug/:slug', verb:'get'},
+          returns: {root: true, type: 'Object'}
+        }
+    ); 
 };
